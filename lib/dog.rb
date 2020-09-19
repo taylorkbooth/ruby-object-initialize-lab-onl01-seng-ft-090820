2e31e7a3eccb51f38b2cd7b1020c6b89772bca8c
Taylor Booth <taylorkbooth@gmail.com>
2020-09-19 19:49:55 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -4,20 +4,21 @@ class Dog
     @name= name
   end
   
-  def breed=(breed)
-    @breed = breed
-  end
+#   def breed=(breed)
+#     @breed = breed
+#   end
   
-  def name=(name)
-  @name = name
-end
+#   def name=(name)
+#   @name = name
+# end
   
-  def breed
-    @breed
-  end
+#   def breed
+#     @breed
+#   end
 
-def name
-  @name
-end
+# def name
+#   @name
+# end
+
 
 end
