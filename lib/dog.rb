470b4c3efc0f4694b1fd2a7726c0ab8a5d131fd2
Taylor Booth <taylorkbooth@gmail.com>
2020-09-19 19:50:58 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,27 @@
+class Dog
+  
+  attr_accessor :name, :breed
+  
+  def initialize(name, breed="Mutt")
+    @breed= breed
+    @name= name
+  end
+  
+#   def breed=(breed)
+#     @breed = breed
+#   end
+  
+#   def name=(name)
+#   @name = name
+# end
+  
+#   def breed
+#     @breed
+#   end
+
+# def name
+#   @name
+# end
+
+
+end
